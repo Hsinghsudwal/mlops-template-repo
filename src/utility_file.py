95bff87394e3c83f4f39a35d4700a4b6ee5398a3
Harinder Singh Sudwal <sudwalh@gmail.com>
2024-03-17 19:30:29 -0700

    update files

--- src/utility_file.py
+++ src/utility_file.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+import dill
+import pickle
 
 
 class Utility:
@@ -19,6 +21,30 @@ class Utility:
             # Creating a directory if it does not exist already
             if not os.path.exists(folder_name):
                 os.makedirs(folder_name)
+
+        except Exception as e:
+            raise e
+        
+    def save_model(file_path, obj):
+        """save model to the path folder with name """
+        try:
+            dir_path = os.path.dirname(file_path)
+
+            os.makedirs(dir_path, exist_ok=True)
+
+            with open(file_path, "wb") as file_obj:
+                pickle.dump(obj, file_obj)
+
+        except Exception as e:
+            raise e
+        
+    def load_model(file_path):
+        """this will load the model from save path 
+        return : model name
+        """
+        try:
+            with open(file_path, "rb") as file_obj:
+             return pickle.load(file_obj)
 
         except Exception as e:
             raise e
